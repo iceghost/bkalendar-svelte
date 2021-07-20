2840ac5203685c55a6f90dfaece7404abc521022
Duy Khang <khangemail22@gmail.com>
2021-07-20 18:14:23 +0700

    Add basic offline support

---
+++ src/service-worker/index.ts
@@ -0,0 +1,45 @@
+/// <reference lib="webworker" />
+
+import { build, files, timestamp } from '$service-worker';
+
+const worker = (self as unknown) as ServiceWorkerGlobalScope;
+const cacheName = `cache${timestamp}`;
+
+worker.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(build.concat(files));
+    })(),
+  );
+});
+
+worker.addEventListener('fetch', (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
+  );
+});
+
+worker.addEventListener('activate', (e) => {
+  e.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      for (const key of keys) {
+        if (key !== cacheName) await caches.delete(key);
+      }
+    })(),
+  );
+});
